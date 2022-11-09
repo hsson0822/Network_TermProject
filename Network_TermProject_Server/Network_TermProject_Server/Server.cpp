241c850e0c31b33471c3ebc8af35240438776cf7
Lee Dong Hyun <aopo3948@gmail.com>
2022-11-09 11:00:23 +0900

    Server 기초
    소켓 설정 및 Accept
    Accept 이후 클라이언트 별 쓰레드를 만들어 recv하도록 설정
    client, object 클래스를 만들어 각 클라이언트, 오브젝트를 서버에서 관리하도록 함
    각 클라이언트, 오브젝트는 array로 관리

---
+++ Network_TermProject_Server/Network_TermProject_Server/Server.cpp
@@ -0,0 +1,143 @@
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <iostream>
+#include <array>
+
+#pragma comment(lib,"ws2_32")
+
+constexpr unsigned short PORT = 9000;
+constexpr int BUF_SIZE = 5000;
+
+// 클라이언트의 정보가 담김
+class client {
+private:
+	short x, y;		// x,y 좌표
+	short size;		// 물고기 크기
+
+public:
+	void SetX(short pos_x) { x = pos_x; }
+	void SetY(short pos_y) { y = pos_y; }
+	void SetSize(short size) { size = size; }
+
+	short GetX() const { return x; };
+	short GetY() const { return y; };
+	short GetSize() const { return size; }
+};
+
+// 오브젝트들의 정보가 담김
+class object {
+private:
+	short x, y;		// 오브젝트 좌표
+	char type;		// 오브젝트 종류
+
+public:
+	void SetX(short pos_x) { x = pos_x; }
+	void SetY(short pos_y) { y = pos_y; }
+	void SetSize(short type) { type = type; }
+
+	short GetX() const { return x; };
+	short GetY() const { return y; };
+	short GetType() const { return type; }
+};
+
+std::array<client, 3> clients;		// 클라이언트들의 컨테이너
+std::array<object, 100> objects;	// 오브젝트 컨테이너
+
+// 오류 검사용 함수
+void err_display(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+
+// 클라이언트 별 쓰레드 생성
+DWORD WINAPI RecvThread(LPVOID arg)
+{
+	int retval;
+	SOCKET client_socket = reinterpret_cast<SOCKET>(arg);
+
+	int len{};
+	char buf[BUF_SIZE];
+
+	while (true) {
+		retval = recv(client_socket, buf, BUF_SIZE, MSG_WAITALL);
+		if (SOCKET_ERROR == retval) {
+			err_display("file size recv()");
+			return 0;
+		}
+		else if (0 == retval) {
+			return 0;
+		}
+	}
+
+	closesocket(client_socket);
+
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (INVALID_SOCKET == listen_sock) {
+		err_display("socket()");
+		return 1;
+	}
+
+	int retval;
+	sockaddr_in serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(PORT);
+
+	retval = bind(listen_sock, reinterpret_cast<sockaddr*>(&serveraddr), sizeof(serveraddr));
+	if (SOCKET_ERROR == retval) {
+		err_display("bind()");
+		return 1;
+	}
+
+	retval = listen(listen_sock, SOMAXCONN);
+	if (SOCKET_ERROR == retval) {
+		err_display("listen()");
+		return 1;
+	}
+
+	SOCKET client_socket;
+	sockaddr_in addr;
+	int addrlen;
+	HANDLE hThread;
+
+	while (true) {
+
+		// 3명을 받으면 accept 를 종료하고 다른일을 하도록 추가할 예정
+		// 3명이 게임 시작을 눌렀는지 검사하는 과정이 필요함
+
+		addrlen = sizeof(addr);
+		client_socket = accept(listen_sock, reinterpret_cast<sockaddr*>(&addr), &addrlen);
+		if (INVALID_SOCKET == client_socket) {
+			err_display("accept()");
+			break;
+		}
+
+		hThread = CreateThread(nullptr, 0, RecvThread,
+			reinterpret_cast<LPVOID>(client_socket), 0, nullptr);
+		if (!hThread)
+			closesocket(client_socket);
+		else
+			CloseHandle(hThread);
+	}
+
+	closesocket(listen_sock);
+	WSACleanup();
+}
